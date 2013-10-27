a68994ff1d6b9d5ef7e7e2a2bc8bc984376c0d00
Dominik Krejcik <dominik@inmite.eu>
2013-10-27 01:52:54 +0100

    Stuff

--- server/public/js/controllers/deadlinesCtrl.js
+++ server/public/js/controllers/deadlinesCtrl.js
@@ -8,6 +8,7 @@
 todomvc.controller('DeadlinesCtrl', function DeadlinesCtrl($scope, $location, deadlineService, filterFilter) {
 	deadlineService.get(function (data) {
 		deadlineService.getModules(function (mods) {
+			mods.push({"name": "Add module...", "key": null});
 			$scope.addingDeadline = false;
 			var deadlines = $scope.deadlines = data;
 			var modules = $scope.modules = mods;
@@ -21,26 +22,36 @@ todomvc.controller('DeadlinesCtrl', function DeadlinesCtrl($scope, $location, de
 			$scope.$watch('deadlines', function (newValue, oldValue) {
 				if (newValue.length > oldValue.length) {
 					deadlineService.post(newValue[newValue.length-1]);
+					$scope.deadlines = _.sortBy(newValue, function(item) {return item.dueDate;});
 				}
 			}, true);
 
 			$scope.addClicked = function () {
 				$scope.addingDeadline = true;
 			};
-
 			$scope.addDeadline = function () {
+				if (!$scope.newDModule || !$scope.newDTask || !$scope.newDDue.key) {
+					return false;
+				}
 				var module = $scope.newDModule;
 				var name = $scope.newDTask;
-				var due = $scope.newDDue;
+				var dueDate = moment($scope.newDDue, "DD/MM/YY HH:mm")._d;
+				var due = moment(dueDate).fromNow() + " (" + moment(dueDate).format("DD/MM/YY HH:mm") + ")";
 
 				deadlines.push({
 					module: module,
 					name: name,
-					due: due
+					due: due,
+					dueDate: dueDate
 				});
 
 				$scope.newDModule = $scope.newDTask = $scope.newDDue = '';
 				$scope.addingDeadline = false;
+			};
+			$scope.moduleChanged = function () {
+				if ($scope.newDModule.key === null) {
+
+				}
 			};
 		});
 	});
--- server/public/js/services/deadlineService.js
+++ server/public/js/services/deadlineService.js
@@ -5,13 +5,20 @@ var deadlineService = function($http) {
         get: function (callback) {
             $http({method: 'GET', url: 'http://127.0.0.1:4567/deadlines'}).
             success(function(data, status, headers, config) {
+                for (var i in data) {
+                    var deadline = data[i];
+                    var dueDate = Date.parse(deadline.due);
+                    deadline.due = moment(dueDate).fromNow() + " (" + moment(dueDate).format("DD/MM/YY HH:mm") + ")" ;
+                    deadline.dueDate = dueDate;
+                }
+                data = _.sortBy(data, function (item) {return item.dueDate;});
                 callback(data);
             }).
             error(function(data, status, headers, config) {
                 callback([]);
             });
         }, post: function (data) {
-            $http({method: 'POST', url: 'http://127.0.0.1:4567/deadlines', data: {module: data.module.key, name: data.name, due: data.due}}).
+            $http({method: 'POST', url: 'http://127.0.0.1:4567/deadlines', data: {module: data.module.key, name: data.name, due: data.dueDate}}).
             success(function(data, status, headers, config) {
                 console.log("Yay!");
             }).
